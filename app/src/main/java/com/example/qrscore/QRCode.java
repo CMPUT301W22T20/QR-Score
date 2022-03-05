0d2843d9fe7475c777a7d15715ddf2d2c8a184c1
Matthew <mlam2@ualberta.ca>
2022-03-05 13:18:48 -0700

    Basic get/set/read functionality for QRCode class

--- app/src/main/java/com/example/qrscore/QRCode.java
+++ app/src/main/java/com/example/qrscore/QRCode.java
@@ -16,4 +16,36 @@ public class QRCode {
         this.hasScanned = new ArrayList<>();
         this.comments  = new ArrayList<>();
     }
+
+    public void addScanned(Player player) {
+        hasScanned.add(player);
+    }
+
+    public void deleteScanned(Player player) {
+        hasScanned.remove(player);
+    }
+
+    public boolean findScanned(Player player) {
+        return hasScanned.contains(player);
+    }
+
+    public ArrayList<Player> getScanned() {
+        return hasScanned;
+    }
+
+    public void addComment(String comment) {
+        comments.add(comment);
+    }
+
+    public void deleteComment(String comment) {
+        comments.remove(comment);
+    }
+
+    public boolean findComment(String comment) {
+        return comments.contains(comment);
+    }
+
+    public ArrayList<String> getComments() {
+        return comments;
+    }
 }
