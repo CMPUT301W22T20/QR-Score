b6aebf5dca3b4f200fb1b896875a255f6009a04f
akaujla <akaujla@ualberta.ca>
2022-03-13 14:19:01 -0600

    working on profile

--- app/src/main/java/com/example/qrscore/OtherPlayerProfileFragment.java
+++ app/src/main/java/com/example/qrscore/OtherPlayerProfileFragment.java
@@ -10,18 +10,18 @@ import androidx.fragment.app.Fragment;
 
 // TODO: go to this activity when view profile is clicked on for player
 
-public class OtherPlayerQRCodesFragment extends Fragment {
+public class OtherPlayerProfileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
+    private Account ARG_PARAM1 = account;
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
+    private Account account;
     private String mParam2;
 
-    public OtherPlayerQRCodesFragment() {
+    public OtherPlayerProfileFragment() {
         // Required empty public constructor
     }
 
@@ -29,15 +29,15 @@ public class OtherPlayerQRCodesFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
+     * @param account Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment LeaderboardFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LeaderboardFragment newInstance(String param1, String param2) {
+    public static OtherPlayerProfileFragment newInstance(Account account, String param2) {
         LeaderboardFragment fragment = new LeaderboardFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM1, account);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
