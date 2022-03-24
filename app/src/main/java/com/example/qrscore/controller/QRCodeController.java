b7319317903b402a0047712b552aeb7ec9e092dc
Matthew <mlam2@ualberta.ca>
2022-03-24 16:05:05 -0600

    Integrate QR delete with Firestore + update scores appropriately

--- app/src/main/java/com/example/qrscore/controller/QRCodeController.java
+++ app/src/main/java/com/example/qrscore/controller/QRCodeController.java
@@ -6,6 +6,7 @@ import com.example.qrscore.model.QRCode;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -31,15 +32,15 @@ public class QRCodeController {
 
     /**
      * Purpose: To add a QR Code to firestore db.
-     * @param key
+     * @param hash
      *      The hash of the QR Code.
      * @param qrCode
      *      Instance of the QR Code.
      * @param uuid
      *      User UID of user that added it.
      */
-    public void add(String key, QRCode qrCode, String uuid) {
-        QRCodeColRef.document(key).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+    public void add(String hash, QRCode qrCode, String uuid) {
+        QRCodeColRef.document(hash).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
@@ -51,14 +52,13 @@ public class QRCodeController {
                         }
                     }
                     else {
-                        QRCodeColRef.document(key).set(qrCode);
+                        QRCodeColRef.document(hash).set(qrCode);
                     }
                 }
             }
         });
         accountColRef.document(uuid).update("QRCodes", FieldValue.arrayUnion(qrCode.getHash()));
 
-        // TODO: Add functionality to update Score & Total in FireBase Account collection
         accountColRef.document(uuid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -66,6 +66,33 @@ public class QRCodeController {
                     DocumentSnapshot accDoc = task.getResult();
                     Integer score = Integer.parseInt(accDoc.getString("Score"))+qrCode.getQRScore();
                     Integer total = Integer.parseInt(accDoc.getString("Total"))+1;
+                    accountColRef.document(uuid).update("Score", score.toString());
+                    accountColRef.document(uuid).update("Total", total.toString());
+                }
+            }
+        });
+    }
+
+    public void remove(String hash, QRCode qrCode, String uuid) {
+        if (qrCode == null) {
+            throw new IllegalArgumentException("No valid QRCode was passed into this function.");
+        }
+
+        // Get document references
+        DocumentReference qrCodeRef = db.collection("QRCode").document(hash);
+        DocumentReference accountRef = db.collection("Account").document(uuid);
+
+        // Remove code from hasScanned and Account
+        qrCodeRef.update("scanned", FieldValue.arrayRemove(uuid));
+        accountRef.update("QRCodes", FieldValue.arrayRemove(hash));
+
+        accountColRef.document(uuid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot accDoc = task.getResult();
+                    Integer score = Integer.parseInt(accDoc.getString("Score"))-qrCode.getQRScore();
+                    Integer total = Integer.parseInt(accDoc.getString("Total"))-1;
                     accountColRef.document(uuid).update("Score", score.toString());
                     accountColRef.document(uuid).update("Total", total.toString());
                 }
