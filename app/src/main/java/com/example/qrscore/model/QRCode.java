d362f38a2be28f116320ee14d42e3a03db78a3f7
Matthew Braun <mjbraun@ualberta.ca>
2022-04-04 16:53:52 -0600

    Formatting

--- app/src/main/java/com/example/qrscore/model/QRCode.java
+++ app/src/main/java/com/example/qrscore/model/QRCode.java
@@ -53,7 +53,7 @@ public class QRCode {
      *     Formatted string
      */
     public String appendZeroes(String value) {
-        Integer numZeroesToPrefix = 8 - value.length();
+        Integer numZeroesToPrefix = 4 - value.length();
         String appendedValue = "";
         for (int i = 0; i < numZeroesToPrefix; i++) {
             appendedValue += "0";
