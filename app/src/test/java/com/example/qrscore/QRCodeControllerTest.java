25f4890462b4f6c437cfce1f7b342e81f5962050
Matthew Braun <mjbraun@ualberta.ca>
2022-04-04 13:19:47 -0600

    updates

--- app/src/test/java/com/example/qrscore/QRCodeControllerTest.java
+++ app/src/test/java/com/example/qrscore/QRCodeControllerTest.java
@@ -1,10 +1,20 @@
 package com.example.qrscore;
 
+import com.example.qrscore.controller.AccountController;
+import com.example.qrscore.controller.QRCodeController;
+import com.example.qrscore.model.Account;
+
+import org.checkerframework.checker.units.qual.A;
 import org.junit.Test;
 
+private class MockAccount extends Account {
+
+}
+
 public class QRCodeControllerTest {
-
-
+    private QRCodeController qrCodeController = new QRCodeController();
+    private Account account = new Account("TestAccount");
+    private AccountController accountController = new AccountController();
 
     @Test
     public void testAddQRCode() {
