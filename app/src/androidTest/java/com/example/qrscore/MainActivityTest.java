f207ac995690af3564d831be4e7b1b4758b28a3d
William Liu <william.liu4518@gmail.com>
2022-04-04 00:31:35 -0600

    updated tests for qr login and qr view game status

--- app/src/androidTest/java/com/example/qrscore/MainActivityTest.java
+++ app/src/androidTest/java/com/example/qrscore/MainActivityTest.java
@@ -16,6 +16,7 @@ import androidx.test.rule.ActivityTestRule;
 import com.example.qrscore.activity.MainActivity;
 import com.example.qrscore.activity.OtherPlayerAccountActivity;
 import com.example.qrscore.controller.ProfileController;
+import com.example.qrscore.fragment.QRGeneratorDialog;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -41,6 +42,7 @@ public class MainActivityTest {
 
     @Test
     public void testLogin() {
+        // Assumes User is Created.
         solo.clickOnView(solo.getView(R.id.auth_returning_user_button));
         solo.waitForText("Login Successful!", 1, 2000);
     }
@@ -48,11 +50,21 @@ public class MainActivityTest {
     @Test
     public void testHomeFragment() {
         solo.clickOnView(solo.getView(R.id.home_fragment_item));
+
+        solo.clickOnView(solo.getView(R.id.home_fragment_actionbar_qr_code));
+        solo.waitForFragmentById(R.id.qr_generator_dialog, 2000);
     }
 
     @Test
     public void testMapFragment() {
         solo.clickOnView(solo.getView(R.id.map_fragment_item));
+
+        solo.assertCurrentActivity("Wrong activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.map_fragment_item));
+
+        solo.enterText(0, "12");
+        solo.enterText(1, "12");
+        solo.clickOnButton("Search");
     }
 
     @Test
@@ -102,14 +114,26 @@ public class MainActivityTest {
         // Save profile
         solo.clickOnView(solo.getView(R.id.profile_save_button));
         solo.waitForText("Profile has been updated!", 1, 2000);
+
         // Click on other fragment and check that profile has not changed.
         solo.clickOnView(solo.getView(R.id.leaderboard_fragment_item));
         solo.clickOnView(solo.getView(R.id.profile_fragment_item));
-        solo.searchText("Test");
-        solo.searchText("Case");
-        solo.searchText("testcase@qrscore.io");
-        solo.searchText("(780) 301-2022");
-        solo.searchText(profileController.getProfile().getUserUID());    // Checks for UserUID/username
+
+        EditText usernameET = solo.getEditText(profileController.getProfile().getUserUID());
+        EditText firstNameET = solo.getEditText("Test");
+        EditText lastNameET = solo.getEditText("Case");
+        EditText emailET = solo.getEditText("testcase@qrscore.io");
+        EditText phoneNumberET = solo.getEditText("301-2022");
+
+        assertEquals("Test", firstNameET.getText().toString());
+        assertEquals("Case", lastNameET.getText().toString());
+        assertEquals("testcase@qrscore.io", emailET.getText().toString());
+        assertEquals("(780) 301-2022", phoneNumberET.getText().toString());
+        assertEquals(profileController.getProfile().getUserUID(), usernameET.getText().toString());
+
+        // Check QR Code generator to login other devices
+        solo.clickOnView(solo.getView(R.id.profile_generateQR_button));
+        solo.waitForFragmentById(R.id.qr_generator_dialog, 2000);
     }
 
     @After
